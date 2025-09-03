ba492cb43957940d99cece553a05e4477574406b
Kaelmur <a.murataliev@list.ru>
2025-09-03 10:42:21 +0500

    check

--- frontend/src/pages/Admin/MapPage.tsx
+++ frontend/src/pages/Admin/MapPage.tsx
@@ -64,7 +64,10 @@ function MapPage() {
         >
           <LayersControl position="topright">
             <LayersControl.BaseLayer checked name="Светлая">
-              <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
+              <TileLayer
+                url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                maxZoom={22}
+              />
             </LayersControl.BaseLayer>
             <LayersControl.BaseLayer name="Темная">
               <TileLayer url="https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png" />
